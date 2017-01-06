2d451c9227468cadf830859d7359a6de32fde69d
toert <pw177@mail.ru>
2017-01-07 01:58:04 +0300

    fix newline bug

--- typograph.py
+++ typograph.py
@@ -1,15 +1,16 @@
-from re import compile
+from re import sub
 
 
 def compile_patterns():
     patterns = [
-        (compile(r'(["\'])(.*?)(\1)'), r'«\2»'), # ёлочки
-        (compile(r'( - )'), ' — '), # Дефисы с пробелами на тире
-        (compile(r'(\d{1,4})-'), r'\1–'), # номера телефонов и промежуток лет
-        (compile(r'(\d)-([^\W|\d])'), r'\1–\2'), #связывание чисел с последующими словами неразрывным дефисом
-        (compile(r'[ ]+'), r' '), # удаление лишних пробелов
-        (compile(r'\n+'), r'\n'), # удаление лишних пустых строк
-        (compile(r'(\b[^\W|\d]{1,2}) (\w)'), r'\1 \2'),  # связывание слов из 1-2 символов
+        (r'(["\'])(.*?)(\1)', r'«\2»'), # ёлочки
+        (r'( - )', ' — '), # Дефисы с пробелами на тире
+        (r'(\d{1,4})-', r'\1–'), # номера телефонов и промежуток лет
+        (r'(\d)-([^\W|\d])', r'\1–\2'), #связывание чисел с последующими словами неразрывным дефисом
+        (r'[ ]{2,}', r' '), # удаление лишних пробелов
+        (r'\r\n', r'\n'),  # удаление лишних пустых строк Windows
+        (r'\n{2,}', r'\n'),  # удаление лишних пустых строк Unix
+        (r'(\b[^\W|\d]{1,2}) (\w)', r'\1 \2'),  # связывание слов из 1-2 символов
 
     ]
     return patterns
@@ -17,8 +18,8 @@ def compile_patterns():
 
 def typo_text(text):
     patterns = compile_patterns()
-    for pattern in patterns:
-        text = pattern[0].sub(pattern[1], text)
+    for what_is_substituded, for_what in patterns:
+        text = sub(what_is_substituded, for_what, text)
     return text
 
 
